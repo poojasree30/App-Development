9a35347cf3b92b3b87266995b14306a631153f33
poojasree30 <120357209+poojasree30@users.noreply.github.com>
2023-10-11 11:54:23 +0530

    Delete microservice/microservice directory

--- microservice/microservice/src/main/java/com/example/microservice/repository/feedbackrepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.microservice.repository;
-
-
-import com.example.microservice.entity.feedback;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface feedbackrepo extends JpaRepository<feedback ,Integer> {
-
-}
